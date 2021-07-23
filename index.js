0e246034fd3488d36c06e02c4594b45c3b2d1ca7
Brayan Hernandez <brayan_hernandez@gmail.com>
2021-07-23 14:48:20 -0500

    Getting the countries from an api

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log('Hola mundo')
 console.log("This is something I'm adding this is brayan");
+console.log("This is something I'm adding this is brayan");
+
+const getCountries = () => {
+    console.log("This function basically retuns the countries");
+}
