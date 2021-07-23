739c84820c59f7d5cf09c53c026ed8f0aeb81f96
Brayan Hernandez <brayan_hernandez@gmail.com>
2021-07-23 15:49:51 -0500

    Rebase from main

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 console.log('Hola mundo')
 console.log("This is something I'm adding this is brayan");
-console.log("This is something I'm adding this is brayan");
 
 const getCountries = () => {
     console.log("This function basically retuns the countries");
     console.log("This is something I'm adding this is brayan");
 
-    const getAllClients = () => {
-        console.log('We are just adding something here');
-    }
+}
+
+const getAllClients = () => {
+    console.log('We are just adding something here');
+}
