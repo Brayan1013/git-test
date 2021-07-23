a315042a5fa5d73b05b6bdd95e39c293809cb51a
Brayan Hernandez <39144645+Brayan1013@users.noreply.github.com>
2021-07-23 14:46:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ console.log("This is something I'm adding this is brayan");
 
 const getCountries = () => {
     console.log("This function basically retuns the countries");
-}
+    console.log("This is something I'm adding this is brayan");
+
+    const getAllClients = () => {
+        console.log('We are just adding something here');
+    }
